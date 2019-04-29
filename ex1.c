d9326108b946df993e9f749ca6dadd0682246f30
ChenMing <chenming@192.168.0.100>
2019-04-30 06:07:27 +0800

    update

---
+++ ex1.c
@@ -0,0 +1,15 @@
+# include <stdio.h>
+
+/* this is the comment */
+
+int main(int argc, char *argv[]){
+
+  int distance = 100;
+
+    // this is also a comment
+
+  printf("you are %d miles away from home.\n", distance);
+
+  return 0;
+
+}
---
+++ ex2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+
+  int age= 30;
+  int height = 172;
+
+  printf("you are %d years old.\n", age);
+  printf("I'm %d cm tall.\n", height);
+
+  return 0;
+
+}
